7465b7e27a6d77bc33ed19aae9baffd20a0faf8d
Wallison Storck <storck779@gmail.com>
2022-11-26 12:21:14 -0400

    Unified sideMenu CSS

---
+++ TO-DO.md
@@ -0,0 +1,11 @@
+# A fazer:
+
+- Adicionar tradução
+
+- Adicionar seleção estilizada no menu clicado.
+
+  - Quando clicar no menu ele deve ficar em negrito.
+  - Ao rolar a pagina a seleção deve ser desfeita.
+  - Menu lateral deve ser escondido.
+
+- Ajustar imagens (retângulos) de fundo para que diminuam um pouco com resoluções menores.
--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-window.onload = () => {
+function sideMenu() {
   // AJUSTES DO MENU INTERATIVO
   const menu = document.querySelector(".menu .links");
   const menuIcon = document.querySelector(".side-menu .menu-icon");
 
   menuIcon.addEventListener("click", () => {
-    menu.classList.toggle("active");
+    menu.classList.toggle("sideMenuActive");
   });
+}
+
+window.onload = () => {
+  sideMenu();
 };
