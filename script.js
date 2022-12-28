a0bf439e1142ee88512818ab41df6c5d74c319e4
Wallison Storck <storck779@gmail.com>
2022-12-28 16:38:28 -0400

    Added function to remove menu on logo click

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 let elements = {
   menu: "",
   menuIcon: "",
+  logoIcon: "",
 };
 
 function removeSideMenuAfterClick() {
-  // AO CLICAR NO MENU ELE ESCONDE
-  elements.menu.addEventListener("click", () => {
-    elements.menu.classList.remove("sideMenuActive");
+  // AO ABRIR A APLICAÇÃO, ADICIONA EVENTO DE REMOVER MENU, NOS ITENS DO PRÓPRIO MENU E NA LOGO
+  [elements.logoIcon, elements.menu].forEach((element) => {
+    element.addEventListener("click", () => {
+      elements.menu.classList.remove("sideMenuActive");
+    });
   });
 }
 
 function btnSideMenu() {
-  // AJUSTES DO MENU INTERATIVO
-  elements.menu = document.querySelector(".menu .links");
-  elements.menuIcon = document.querySelector(".side-menu .menu-icon");
+  // ADICIONA EVENTO DE ACIONAR E ESCONDER O MENU ATRAVÉS DAS BARRAS
+  const header = document.querySelector("header");
+  elements.menu = header.querySelector(".menu .links"); //Guarda todos o menu
+  elements.menuIcon = header.querySelector(".side-menu .menu-icon"); //Guarda referencia das "barrinhas"
+  elements.logoIcon = header.querySelector(".menu .logo");
 
   elements.menuIcon.addEventListener("click", () => {
+    //Adiciona o vento de ao clicar nas "barrinhas" adiciona ou remove o menu
     elements.menu.classList.toggle("sideMenuActive");
   });
 }
